23336c0cfd3e71301d6a3f2b605b88578ccea2f0
WillCunha <willianfernandocunha932@gmail.com>
2024-07-14 12:29:12 -0300

    Versão API: 14 de Julho de 2024.

--- api/Engine/Controller.php
+++ api/Engine/Controller.php
@@ -8,8 +8,54 @@ if ($acao == '') {
     echo json_encode(["Erro:" => "Ação não permitida"]);
 } else if ($acao != '' && $param != '') {
     if ($acao == 'gera-nfe') {
-        //$dadosIngressos = GeraNfe::GeraNFE($param);
-        //$insereDados =  new InsereVenda($param);
-        //echo json_encode(["dados" => $insereDados]);
+        $fp = fopen('php://input', 'r');
+        $rawData = stream_get_contents($fp);
+        $data =  json_decode($rawData);
+        new InsereVenda($data);
+        $arquivo = fopen('qualquercoisa.json', 'w');
+        fwrite($arquivo, $rawData);
+        fclose($arquivo);
+
+        $geraNfe = new GeraNfe;
+        geraNfe::$chave = $data->chave;
+        geraNfe::$razaoSocialEmissor = $data->razaoSocialEmissor;
+        geraNfe::$ieEmissor = $data->ieEmissor;
+        geraNfe::$cnpjEmisor = $data->cnpjEmissor;
+        geraNfe::$siglaUFEmissor = $data->ufEmissor;
+        geraNfe::$ruaEmissor = $data->ruaEmissor;
+        geraNfe::$numeroRuaEmissor = $data->numeroRuaEmissor;
+        geraNfe::$nomeBairroEmissor = $data->nomeBairroEmissor;
+        geraNfe::$codigoMunicipioEmissor = $data->codigoMunicipioEmissor;
+        geraNfe::$nomeCidadeEmissor = $data->nomeCidadeEmissor;
+        geraNfe::$cepEmissor = $data->cepEmissor;
+        geraNfe::$codigoPaisEmissor = $data->codigoPaisEmissor;
+        geraNfe::$nomePaisEmissor = $data->nomePaisEmissor;
+        
+        geraNfe::$razaoSocialDest = $data->razaoSocialEmissorDest;
+        geraNfe::$indIEDest = $data->indIEDest;
+        geraNfe::$cnpjDest = $data->cnpjDest;
+        geraNfe::$ruaDest = $data->ruaDest;
+        geraNfe::$numeroRuaDest = $data->numeroRuaDet;
+        geraNfe::$nomeBairroDest = $data->nomeBairroDest;
+        geraNfe::$codigoMunicipioDest = $data->codigoMunicipioDest;
+        geraNfe::$nomeCidadeDest = $data->nomeCidadeDest;
+        geraNfe::$siglaUFDest = $data->siglaUFDest;
+        geraNfe::$cepDest = $data->cepEmissorDest;
+        
+        geraNfe::$arrayProdutos = $data->produtos;
+
+        geraNfe::$modFrete = $data->modFrete;
+        geraNfe::$quantidadeVol = $data->quantidadeVol;
+        geraNfe::$especie = $data->especie;
+        geraNfe::$marcaCaixa = $data->marcaCiaxa;
+        geraNfe::$peso = $data->pesoLiquido;
+        geraNfe::$quantidadeParcelas = $data->quantidadeParcelas;
+        $dados = GeraNfe::GeraNFE();
+        //$venda->quantidade = $data->quantidade;
+        //$atualiza = $venda->atualiza();
+        echo json_encode(["dados" => $insereDados]);
+    }
+    else if ($acao == 'assina'){
+        
     }
 }
--- api/Engine/listener.php
+++ api/Engine/listener.php
@@ -6,54 +6,9 @@ use App\GeraNfe;
 use App\InsereVenda;
 
 if ($api == 'gera') {
-    if ($method == 'GET') {
+    if ($method == 'POST') {
         require_once('Controller.php');
     }else{
-        require_once('Controller.php');
-        $fp = fopen('php://input', 'r');
-        $rawData = stream_get_contents($fp);
-        $data =  json_decode($rawData);
-        new InsereVenda($data);
-        $arquivo = fopen('qualquercoisa.json', 'w');
-        fwrite($arquivo, $rawData);
-        fclose($arquivo);
-
-        $geraNfe = new GeraNfe;
-        geraNfe::$chave = $data->chave;
-        geraNfe::$razaoSocialEmissor = $data->razaoSocialEmissor;
-        geraNfe::$ieEmissor = $data->ieEmissor;
-        geraNfe::$cnpjEmisor = $data->cnpjEmissor;
-        geraNfe::$siglaUFEmissor = $data->ufEmissor;
-        geraNfe::$ruaEmissor = $data->ruaEmissor;
-        geraNfe::$numeroRuaEmissor = $data->numeroRuaEmissor;
-        geraNfe::$nomeBairroEmissor = $data->nomeBairroEmissor;
-        geraNfe::$codigoMunicipioEmissor = $data->codigoMunicipioEmissor;
-        geraNfe::$nomeCidadeEmissor = $data->nomeCidadeEmissor;
-        geraNfe::$cepEmissor = $data->cepEmissor;
-        geraNfe::$codigoPaisEmissor = $data->codigoPaisEmissor;
-        geraNfe::$nomePaisEmissor = $data->nomePaisEmissor;
-        
-        geraNfe::$razaoSocialDest = $data->razaoSocialEmissorDest;
-        geraNfe::$indIEDest = $data->indIEDest;
-        geraNfe::$cnpjDest = $data->cnpjDest;
-        geraNfe::$ruaDest = $data->ruaDest;
-        geraNfe::$numeroRuaDest = $data->numeroRuaDet;
-        geraNfe::$nomeBairroDest = $data->nomeBairroDest;
-        geraNfe::$codigoMunicipioDest = $data->codigoMunicipioDest;
-        geraNfe::$nomeCidadeDest = $data->nomeCidadeDest;
-        geraNfe::$siglaUFDest = $data->siglaUFDest;
-        geraNfe::$cepDest = $data->cepEmissorDest;
-        
-        geraNfe::$arrayProdutos = $data->produtos;
-
-        geraNfe::$modFrete = $data->modFrete;
-        geraNfe::$quantidadeVol = $data->quantidadeVol;
-        geraNfe::$especie = $data->especie;
-        geraNfe::$marcaCaixa = $data->marcaCiaxa;
-        geraNfe::$peso = $data->pesoLiquido;
-        geraNfe::$quantidadeParcelas = $data->quantidadeParcelas;
-        $dados = GeraNfe::GeraNFE();
-        //$venda->quantidade = $data->quantidade;
-        //$atualiza = $venda->atualiza();
+        echo json_encode(["Erro: " => "Não é permitido esse tipo de entrada aqui!"]);
     }
 }
